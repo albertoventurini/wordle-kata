79151093679966ab66b6833f07a2ba65564998ab
Alberto Venturini <aventurini@gmail.com>
2022-02-18 16:54:26 +0200

    Implement 'getWord' method

--- src/main/java/com/spandigital/wordlekata/WordGenerator.java
+++ src/main/java/com/spandigital/wordlekata/WordGenerator.java
@@ -1,16 +1,26 @@
 package com.spandigital.wordlekata;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 public class WordGenerator {
 
-    Set<String> words;
+    private final List<String> words;
 
-    public WordGenerator(Set<String> wordsIn) {//
-        words = wordsIn;
+    private Random random = new Random();
+
+    public WordGenerator(final Set<String> words) {
+        if (words.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.words = new ArrayList<>(words);
     }
 
-    String getWordOrSomename() {
-        return null;
+    public String getWord() {
+        int index = random.nextInt(words.size());
+        return words.get(index);
     }
 }
--- src/test/java/com/spandigital/wordlekata/WordGeneratorTest.java
+++ src/test/java/com/spandigital/wordlekata/WordGeneratorTest.java
@@ -3,21 +3,45 @@ package com.spandigital.wordlekata;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WordGeneratorTest {
 
-
+    @Test
     public void constructor_whenInputSetIsEmpty_ThrowsException() {
-
-
-        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new WordGenerator(new HashSet<>());
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new WordGenerator(Collections.emptySet());
         });
+    }
 
-        Assertions.assertEquals("some message", thrown.getMessage());
+    @Test
+    public void constructor_whenInputSetIsNotEmpty_DoesntThrowException() {
+        Assertions.assertDoesNotThrow(() -> {
+            new WordGenerator(Set.of("something"));
+        });
+    }
+
+    @Test
+    public void getWord_withSingleWordDictionary_returnsTheWord() {
+        final var wordSet = Set.of("word1");
+        final var wordGenerator = new WordGenerator(wordSet);
+
+        final String word = wordGenerator.getWord();
+
+        assertTrue(wordSet.contains(word));
+    }
+
+    @Test
+    public void getWord_withMultiWordDictionary_returnsAWordInDictionary() {
+        final var wordSet = Set.of("word2", "word3");
+        final var wordGenerator = new WordGenerator(wordSet);
+
+        final String word = wordGenerator.getWord();
+
+        assertTrue(wordSet.contains(word));
     }
 
 }
